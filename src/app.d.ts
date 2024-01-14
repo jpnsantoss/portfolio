dfb569ad0af2239c84b561184667fd8d7c67147f
jpnsantoss <jpnsantos14@gmail.com>
2024-01-14 22:41:22 +0000

    added basic lucia setup

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { PrismaClient } from "@prisma/client/edge";
+
 // for information about these interfaces
 declare global {
 	namespace App {
@@ -8,6 +11,17 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+	// eslint-disable-next-line no-var
+	var __prisma: PrismaClient;
+
+	namespace Lucia {
+		type Auth = import("$lib/server/lucia").Auth;
+		type DatabaseUserAttributes = {
+			username: string;
+		};
+		type DatabaseSessionAttributes = object;
+	}
 }
 
-export {};
+export { };
+
