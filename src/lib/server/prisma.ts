7f42ab3efc0a45d2143adb91deef73f10dcbdd6f
jplayss <jpnhs124@gmail.com>
2024-01-19 16:11:01 +0000

    updated dependencies

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,15 +1,18 @@
 import { dev } from '$app/environment';
-import { PrismaClient } from '@prisma/client/';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 let prisma;
 if (dev) {
     if (!global.__prisma) {
         const client = new PrismaClient();
+        client.$extends(withAccelerate());
         global.__prisma = client;
     }
     prisma = global.__prisma;
 } else {
-    const client = new PrismaClient();;
+    const client = new PrismaClient();
+    client.$extends(withAccelerate());
     prisma = client;
 }
 
