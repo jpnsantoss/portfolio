5a5fbd842bc81096c57c8ab20b3aa64e72b32638
jplayss <jpnhs124@gmail.com>
2024-01-21 15:02:19 +0000

    probably fixed auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,14 +3,10 @@ import client from "$lib/server/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
-import type { Handle } from "@sveltejs/kit";
 
-export const handle = SvelteKitAuth(async () => {
-  const authOptions = {
+export const handle = SvelteKitAuth({
     adapter: PrismaAdapter(client),
     providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
     secret: AUTH_SECRET,
     trustHost: true
-  }
-  return authOptions
-}) satisfies Handle;
+}) ;
