a618ae7ea4fa4ff3837fe13bcf6d1370aee5a14e
jplayss <jpnhs124@gmail.com>
2024-01-21 15:23:17 +0000

    seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client';
+import userData from "../src/lib/data.json" assert { type: "json" };
+
+const prisma = new PrismaClient()
+
+async function main() {
+  console.log(`Start seeding ...`)
+
+  for (const p of userData) {
+    const user = await prisma.authorizedEmail.create({
+      data: {
+        email: p.email,
+      }
+    })
+    console.log(`Added authorized user with email: ${user.email}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
